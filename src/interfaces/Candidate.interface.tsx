b2ecfd7cf294e213a8c3264ce11a0bf93a4fd166
Antonina D <antoninad22@gmail.com>
2025-03-23 11:45:53 -0500

    added Candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,4 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 export interface Candidate {
-
+    login: string;
+    name: string| null;
+    location: string | null;
+    email: string;
+    company: string | null;
+    bio: string | null;
+    avatarUrl: string;
 }
